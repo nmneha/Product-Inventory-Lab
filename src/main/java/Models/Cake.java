bf6ab53a8d28569aea669f35a90bb50b1fff3e25
nusera <nusera.neha@gmail.com>
2022-03-28 12:49:17 -0400

    added properties to cake and ice cream

--- src/main/java/Models/Cake.java
+++ src/main/java/Models/Cake.java
@@ -1,4 +1,11 @@
 package Models;
 
 public class Cake {
+    private int id;
+    private String name;
+    private String brand;
+    private String flavor;
+    private String size;
+    private int qty;
+    private float price;
 }
--- src/main/java/Models/IceCream.java
+++ src/main/java/Models/IceCream.java
@@ -1,4 +1,13 @@
 package Models;
 
 public class IceCream {
+    private int id;
+    private String name;
+    private String brand;
+    private String flavor;
+    private String size;
+    private int qty;
+    private float price;
 }
+
+
