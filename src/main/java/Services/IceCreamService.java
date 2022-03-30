f88d40cbcb31b6822d7008de1b0d6a1cfc09530f
nusera <nusera.neha@gmail.com>
2022-03-29 23:20:00 -0400

    passed find test

--- src/main/java/Services/IceCreamService.java
+++ src/main/java/Services/IceCreamService.java
@@ -1,4 +1,47 @@
 package Services;
 
+import Models.IceCream;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class IceCreamService {
+
+    private static int nextId = 1;
+    private List<IceCream> inventory = new ArrayList<>();
+
+
+    public IceCream create(String brand, String flavor, String size, int qty, double price) {
+        IceCream createdIceCream = new IceCream(nextId++, brand, flavor, size, qty, price);
+        inventory.add(createdIceCream);
+
+        return createdIceCream;
+    }
+
+    //read
+    public IceCream findIceCream(int id) {
+        for (IceCream ic : inventory) {
+            if (ic.getId() == id) {
+                return ic;
+            }
+        }
+
+        return null;
+        // should take an int and return an object with that id, if exists
+    }
+
+    //read all
+    public IceCream[] findAll() {
+        // should return a basic array copy of the ArrayList
+        return null;
+    }
+
+    //delete
+    public boolean delete(int id) {
+        // should remove the object with this id from the ArrayList if exits and return true.
+        // Otherwise return false
+        return false;
+    }
+
+
 }
--- src/test/java/Services/IceCreamServiceTest.java
+++ src/test/java/Services/IceCreamServiceTest.java
@@ -1,7 +1,63 @@
 package Services;
 
+import Models.IceCream;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class IceCreamServiceTest {
 
+    @Test
+    public void createTest() {
+        int id = 1;
+        String flavor = "Bunny BonBon";
+        String brand = "Sera 'an Dipity";
+        String size = "pint";
+        int qty = 12;
+        double price = 6.99;
+
+        IceCreamService bbbSer = new IceCreamService();
+        IceCream bbb = bbbSer.create(brand, flavor, size, qty, price);
+
+        int actualId = bbb.getId();
+        String actualFlavor = bbb.getFlavor();;
+        String actualBrand = bbb.getBrand();
+        String actualSize = bbb.getSize();
+        int actualQty = bbb.getQty();
+        double actualPrice = bbb.getPrice();
+
+        Assertions.assertEquals(Integer.class.getName(), new Integer(actualId).getClass().getName());
+        Assertions.assertEquals(id, actualId);
+        Assertions.assertEquals(brand, actualBrand);
+        Assertions.assertEquals(flavor, actualFlavor);
+        Assertions.assertEquals(size, actualSize);
+        Assertions.assertEquals(qty, actualQty);
+        Assertions.assertEquals(price, actualPrice);
+
+    }
+
+    @Test
+    void findIceCream() {
+        String flavor = "SugarSugar Blue";
+        String brand = "Sera 'an Dipity";
+        String size = "pint";
+        int qty = 12;
+        double price = 6.99;
+
+        IceCreamService ssbSer = new IceCreamService();
+        IceCream ssb =  ssbSer.create(brand, flavor, size, qty, price);
+
+        int actualId = ssb.getId();
+
+        Assertions.assertEquals(ssb, ssbSer.findIceCream(ssb.getId()));
+    }
+
+    @Test
+    void findAll() {
+    }
+
+    @Test
+    void delete() {
+    }
 }
