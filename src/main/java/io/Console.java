7c2186c16dfc1942ae2ef6bea97eea176518b015
nusera <nusera.neha@gmail.com>
2022-04-04 19:50:13 -0400

    print welcom method

---
+++ src/main/java/io/Console.java
@@ -0,0 +1,15 @@
+package io;
+
+public class Console {
+
+    public static void printWelcome(){
+        System.out.println("" +
+                "**************************************************" +
+                "***           Welcome and Bienvenue            ***" +
+                "***                    to                      ***" +
+                "***     Sera 'N' Dipity Inventory Manager      ***" +
+                "**************************************************");
+    }
+
+}
+}
